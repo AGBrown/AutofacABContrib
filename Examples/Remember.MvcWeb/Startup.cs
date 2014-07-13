a46a71584b1187fe0983976d7712b5712a98e664
Andy Brown <andy@andygbrown.co.uk>
2014-07-13 16:28:05 +0100

    Add template Remember.MvcWeb
     - ASP.NET MVC (.NET 4.5.1) project

---
+++ Examples/Remember.MvcWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Remember.MvcWeb.Startup))]
+namespace Remember.MvcWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
