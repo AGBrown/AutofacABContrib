4ed438a6d7058c3d587cc05bd1f47820a55e13ee
Andy Brown <andy@andygbrown.co.uk>
2014-07-13 14:37:33 +0100

    Correct login url
     - Update login url in Web.config
     - Add Authorize to AccountController to test login redirect
     - Set launch page from VS to be `Account/Login`

--- Examples/Remember.Web/Controllers/AccountController.cs
+++ Examples/Remember.Web/Controllers/AccountController.cs
@@ -1,16 +1,21 @@
-﻿using System.Web.Mvc;
+﻿using System.Web;
+using System.Web.Mvc;
 using Remember.Web.Models;
+using Remember.Web.Service;
 
 namespace Remember.Web.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
+        [AllowAnonymous]
         public ActionResult Login(LoginForm model)
         {
             if (ModelState.IsValid)
